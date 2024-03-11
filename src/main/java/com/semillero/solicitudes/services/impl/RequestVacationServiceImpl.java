939d793af88c06024136cbb15034ca2f10dde006
Duvan Labrador <carlosduvanlh@ufps.edu.co>
2024-03-11 14:50:04 -0500

    Added calculated business day and get start date less than 15

--- src/main/java/com/semillero/solicitudes/services/impl/RequestVacationServiceImpl.java
+++ src/main/java/com/semillero/solicitudes/services/impl/RequestVacationServiceImpl.java
@@ -12,6 +12,7 @@ import com.semillero.solicitudes.persistence.repositories.UserRepository;
 import com.semillero.solicitudes.services.interfaces.IRequestVacationService;
 import org.springframework.stereotype.Service;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.List;
@@ -55,29 +56,38 @@ public class RequestVacationServiceImpl implements IRequestVacationService {
         if (periodProbation && !oneYear) {
             int availableDay = calculateDayAvailable(user);
             requestVacation.setNmNumberOfDaysRequested(availableDay);
+
+            LocalDate reinstatementDate = reinstatementDate(requestVacation.getFeStartDate(), availableDay);
+            requestVacation.setFeReinstatementDate(reinstatementDate);
         } else if (oneYear) {
+            if (requestVacation.getNmNumberOfDaysRequested() == null) {
+                throw new ResourceBadRequestException("You must enter the number of vacation days");
+            }
             if (requestVacation.getNmNumberOfDaysRequested() < 6 || requestVacation.getNmNumberOfDaysRequested() > 15) {
                 throw new ResourceBadRequestException("Number of vacation days must be between 6 and 15");
             }
+            LocalDate reinstatementDate = reinstatementDate(requestVacation.getFeStartDate(), requestVacation.getNmNumberOfDaysRequested());
+            requestVacation.setFeReinstatementDate(reinstatementDate);
         } else {
             throw new ResourceBadRequestException("Vacations can only be requested if you have more than 2 months");
         }
+
+        validateVacationRequestDate(requestVacation.getFeStartDate());
 
         RequestVacationEntity requestVacationEntity = createRequest(requestVacation, user);
         RequestVacationEntity requestSaved = this.requestVacationRepository.save(requestVacationEntity);
         return this.requestVacationMapper.requestVacationToRequestVacationDto(requestSaved);
     }
 
-    public void verifyUserExistence(Long id) {
+    private void verifyUserExistence(Long id) {
         if (!this.userRepository.existsById(id)) {
             throw new ResourceNotFoundException("User not found with id: " + id);
         }
     }
 
-    public boolean validateOneYear(UserEntity user) {
+    private boolean validateOneYear(UserEntity user) {
         LocalDate hireDate = user.getEmployeeEntity().getFeHireDate();
         LocalDate currentDate = LocalDate.now();
-        //LocalDate vacationVerify = hireDate.plusYears(1);
 
         Period period = Period.between(hireDate, currentDate);
 
@@ -90,11 +100,11 @@ public class RequestVacationServiceImpl implements IRequestVacationService {
         }
     }
 
-    public boolean validateMonth(UserEntity user) {
+    private boolean validateMonth(UserEntity user) {
         LocalDate hireDate = user.getEmployeeEntity().getFeHireDate();
         LocalDate currentDate = LocalDate.now();
         Period period = Period.between(hireDate, currentDate);
-        System.out.println("MESES " + period.getMonths());
+
         if (period.getYears() == 0 && period.getMonths() > 2) {
             return true;
         } else {
@@ -103,7 +113,7 @@ public class RequestVacationServiceImpl implements IRequestVacationService {
     }
 
 
-    public int calculateDayAvailable(UserEntity user) {
+    private int calculateDayAvailable(UserEntity user) {
         LocalDate hireDate = user.getEmployeeEntity().getFeHireDate();
         LocalDate current = LocalDate.now();
 
@@ -117,6 +127,38 @@ public class RequestVacationServiceImpl implements IRequestVacationService {
         return availableDays;
 
     }
+
+    private boolean isBusinessDay(LocalDate dateAvailable){
+        DayOfWeek dayOfWeek = dateAvailable.getDayOfWeek();
+        return dayOfWeek != DayOfWeek.SATURDAY && dayOfWeek != DayOfWeek.SUNDAY;
+    }
+
+    private LocalDate searchBusinessDay(LocalDate dateAvailable){
+        LocalDate nextDay = dateAvailable.plusDays(1);
+        while(!isBusinessDay(dateAvailable)){
+            nextDay = nextDay.plusDays(1);
+        }
+        return nextDay;
+    }
+
+    private LocalDate reinstatementDate(LocalDate startDate, int availableDays){
+        LocalDate departureDay = startDate.plusDays(availableDays);
+
+        if(isBusinessDay(departureDay)){
+            return  departureDay;
+        }
+        return searchBusinessDay(departureDay);
+    }
+
+    private void validateVacationRequestDate(LocalDate startDate) {
+        LocalDate today = LocalDate.now();
+        Period period = Period.between(today, startDate);
+        if (period.getDays() < 15) {
+            throw new ResourceBadRequestException("Vacation request must be made at least 15 days before the start date");
+        }
+    }
+
+
 
     private RequestVacationEntity createRequest(RequestVacationDto requestVacation, UserEntity user) {
         RequestVacationEntity requestVacationEntity = new RequestVacationEntity();
